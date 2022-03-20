6ef054beede54faf672569495e113a50017876ad
Lacromisss <tu7a45yc6@code.edu.az>
2022-03-20 17:51:54 +0400

    Add project files.

---
+++ ConsoleApp40/Group.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp40
+{
+    internal class Group
+    {
+        public Group(string a,int b)
+        {
+            this._no= a;
+            this._studentLimit= b;
+
+        }
+        private string _no;
+        private int _studentLimit;
+        public string Nooo { get =>
+                this._no;
+                set
+                
+            {
+                if (charstr(value))
+                {
+                    this._no = value;
+
+                }
+
+            }
+        }
+        public int MyProperty { get => this._studentLimit;
+                set 
+            {
+                if (value==15)
+                {
+                    this._studentLimit = value;
+
+                }
+            }
+        }
+        public bool charstr( string a)
+        {
+            if (string.IsNullOrWhiteSpace(a) && a.Length==5)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (char.IsUpper(a[0]) && char.IsUpper(a[1]) && char.IsDigit(a[2]) && char.IsDigit(a[3]))
+                    {
+                        return true;
+
+                    }
+
+                }
+
+            }
+            return false;
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Group[] grp = new Group[2];
+            string inp;
+            int inp2;
+            Console.WriteLine("no daxil edin");
+            do
+            {
+
+         
+            Console.WriteLine("DUZ  daxil et sikerem");
+             inp= Console.ReadLine();
+            } while (!(charstrr(inp) || inp.Length==5 ));
+
+
+            Console.WriteLine("telebe limitini daxil et");
+            do
+            {
+                Console.WriteLine("duz daxil et sikerem");
+                inp2= int.Parse(Console.ReadLine());
+
+            } while (inp2>15);
+            for (int i = 0; i < grp.Length; i++)
+            {
+                grp[i] = new Group(inp, inp2);
+
+            }
+            foreach (var item in grp)
+            {
+                Console.WriteLine($"no{item.Nooo} telebe limiti{item.MyProperty}");
+
+            }
+            
+            
+        }
+        static bool charstrr(string a)
+        {
+            if (string.IsNullOrWhiteSpace(a) && a.Length == 5)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (char.IsUpper(a[0]) && char.IsUpper(a[1]) && char.IsDigit(a[2]) && char.IsDigit(a[3]))
+                    {
+                        return true;
+
+                    }
+
+                }
+
+            }
+            return false;
+
+
+
+
+
+        }
+        
+
+
+
+
+
+
+        
+    }
+}
